ae83a2e2ae8f477c5d70d801525642664aa04cd9
OpuTheCoder <artistop26@gmail.com>
2026-01-18 19:53:56 +0600

    initial commit

--- index.js
+++ index.js
@@ -1,40 +1,34 @@
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
+const products = require('./products.json');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send("Gadget Galaxy Backend is running...");
+});
+
 app.get('/api/products', (req, res) => {
-    fs.readFile('./products.json', 'utf8', (err, data) => {
-        if (err) {
-            res.status(500).send("Data not found");
-        } else {
-            res.json(JSON.parse(data));
-        }
-    });
+    try {
+        res.json(products);
+    } catch (err) {
+        res.status(500).send("Server Error");
+    }
 });
 
 app.get('/api/products/:id', (req, res) => {
     const id = req.params.id;
-    
-    fs.readFile('./products.json', 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).send("Error reading file");
-        }
-        
-        const products = JSON.parse(data);
-        
-        const product = products.find(p => p.id.toString() === id.toString());
+    const product = products.find(p => p.id.toString() === id.toString());
 
-        if (product) {
-            res.json(product);
-        } else {
-            res.status(404).send("Product not found");
-        }
-    });
+    if (product) {
+        res.json(product);
+    } else {
+        res.status(404).send("Product not found");
+    }
 });
 
 app.listen(PORT, () => {
