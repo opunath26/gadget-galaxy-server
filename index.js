9d62156a10381ac8d9b1f534b3016d1e8157540e
OpuTheCoder <artistop26@gmail.com>
2026-01-18 19:48:23 +0600

    commit done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
